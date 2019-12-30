f23789f280dcd14a555e633b707279d9b8b88495
mrtc0 <kohei.morita@pepabo.com>
2019-12-30 16:06:34 +0900

    fix

--- semaphore.sh
+++ semaphore.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Script to test cxray
-#
-# `semaphore.sh` runs the tests in a rkt container with custom build
-# stage1-kvm images to test under different kernels.  stage1-kvm allows
-# us to run a container inside a KVM virtual machine and thus test eBPF
-# workloads, which need a modern Linux kernel and root access.
-
-# from : https://github.com/iovisor/gobpf/blob/master/semaphore.sh
-
-set -eux
+set -eu
 set -o pipefail
 
+# The kernel versions we want to run the tests on
 readonly kernel_versions=("4.9.96")
+
+# The rkt version which is set as a dependency for
+# the custom stage1-kvm images
 readonly rkt_version="1.30.0"
 
+# Directory where this file is, used in the example below
+readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Download rkt if not available yet as Semaphore CI
+# doesn't have rkt at the time of writing
 if [[ ! -f "./rkt/rkt" ]] ||
   [[ ! "$(./rkt/rkt version | awk '/rkt Version/{print $3}')" == "${rkt_version}" ]]; then
 
@@ -27,36 +27,48 @@ fi
 
 # Pre-fetch stage1 dependency due to rkt#2241
 # https://github.com/coreos/rkt/issues/2241
-sudo ./rkt/rkt image fetch --insecure-options=image "coreos.com/rkt/stage1-kvm:${rkt_version}"
+sudo ./rkt/rkt image fetch --insecure-options=image "coreos.com/rkt/stage1-kvm:${rkt_version}" >/dev/null
 
 for kernel_version in "${kernel_versions[@]}"; do
-  kernel_api_header_dir="/lib/modules/${kernel_version}-kinvolk-v1/include:/lib/modules/${kernel_version}-kinvolk-v1/source/include/arch/x86/include"
+  # The stage1-kvm image to use for the tests
+  stage1_name="kinvolk.io/aci/rkt/stage1-kvm:${rkt_version},kernelversion=${kernel_version}"
+
+  # Make sure there's no stale rkt-uuid file
   rm -f ./rkt-uuid
-  sudo timeout --foreground --kill-after=10 10m \
+
+  # You most likely want to provide source code to the
+  # container in order to run the tests. You can do this
+  # with volumes:
+  # https://coreos.com/rkt/docs/latest/subcommands/run.html#mounting-volumes
+
+  # Depending on the level of privileges you need,
+  # `--insecure-options=all-run` might be necessary:
+  # https://coreos.com/rkt/docs/latest/commands.html#global-options
+
+  # timeout can be used to make sure tests finish in
+  # a reasonable amount of time
+  sudo timeout --foreground --kill-after=10 5m \
     ./rkt/rkt \
     run --interactive \
     --uuid-file-save=./rkt-uuid \
-    --insecure-options=image,all-run \
+    --insecure-options=image \
     --dns=8.8.8.8 \
-    --stage1-name="kinvolk.io/aci/rkt/stage1-kvm:${rkt_version},kernelversion=${kernel_version}" \
-    --volume=cxray,kind=host,source="$PWD" \
-    docker://mrtc0/bcc-docker:latest \
-    --memory=1024M \
+    --stage1-name="${stage1_name}" \
+    --volume=cxray,kind=host,source="${dir}" \
     --mount=volume=cxray,target=/go/src/github.com/mrtc0/cxray \
-    --environment=GOPATH=/go \
-    --environment=C_INCLUDE_PATH="${kernel_api_header_dir}" \
-    --environment=BCC_KERNEL_MODULES_SUFFIX="source" \
-    --environment=GO111MODULE=on \
+    docker://mrtc0/bcc-docker:latest \
     --exec=/bin/sh -- -c \
-    'cd /go/src/github.com/mrtc0/cxray &&
-      mount -t tmpfs tmpfs /tmp &&
-      mount -t debugfs debugfs /sys/kernel/debug/ &&
-      make test'
+    'printf "\n\nRUN YOUR TESTS HERE (on kernel $(uname -r))\n\n\n" && 
+     cd /go/src/github.com/mrtc0/cxray &&
+     mount -t tmpfs tmpfs /tmp &&
+     mount -t debugfs debugfs /sys/kernel/debug/ &&
+     go test -v ./...'
 
+  # Determine exit code from pod status due to rkt#2777
+  # https://github.com/coreos/rkt/issues/2777
   test_status=$(sudo ./rkt/rkt status "$(<rkt-uuid)" | awk '/app-/{split($0,a,"=")} END{print a[2]}')
   if [[ $test_status -ne 0 ]]; then
     exit "$test_status"
   fi
-
-  sudo ./rkt/rkt gc --grace-period=0
+  echo "Test successful on ${kernel_version}"
 done
