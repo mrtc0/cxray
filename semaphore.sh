72089300dd51dc875d0829505868476072b20fb4
mrtc0 <kohei.morita@pepabo.com>
2019-12-30 16:45:41 +0900

    add kernel api header path

--- semaphore.sh
+++ semaphore.sh
@@ -30,6 +30,7 @@ fi
 sudo ./rkt/rkt image fetch --insecure-options=image "coreos.com/rkt/stage1-kvm:${rkt_version}" >/dev/null
 
 for kernel_version in "${kernel_versions[@]}"; do
+    kernel_api_header_dir="/lib/modules/${kernel_version}-kinvolk-v1/include:/lib/modules/${kernel_version}-kinvolk-v1/source/include/arch/x86/include:/lib/modules/${kernel_version}-kinvolk-v1/source/include"
   # The stage1-kvm image to use for the tests
   stage1_name="kinvolk.io/aci/rkt/stage1-kvm:${rkt_version},kernelversion=${kernel_version}"
 
@@ -60,6 +61,7 @@ for kernel_version in "${kernel_versions[@]}"; do
     --memory=1024M \
     --environment=GOPATH=/go \
     --environment=GO111MODULE=on \
+    --environment=C_INCLUDE_PATH="${kernel_api_header_dir}" \
     --environment=BCC_KERNEL_MODULES_SUFFIX="source" \
     --exec=/bin/sh -- -c \
     'printf "\n\nRUN YOUR TESTS HERE (on kernel $(uname -r))\n\n\n" && 
